1d26051298e95e2b457055ca364cde4bcc1e00c5
lioncat2002 <sankhayan2002@gmail.com>
2022-04-07 08:25:33 +0530

    kittycall base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import AgoraUIKit from 'agora-react-uikit';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [videoCall, setVideoCall] = useState(true);
+  const rtcProps = {
+    appId: '5d01de05896b4e0ea7ebdacca6d3dc1d',
+    channel: 'test', // your agora channel
+    //token: '<Your channel Token>' // use null or skip if using app in testing mode
+  };
+  const callbacks = {
+    EndCall: () => setVideoCall(false),
+  };
+  return videoCall ? (
+    <div style={{display: 'flex', width: '100vw', height: '100vh'}}>
+      <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} />
     </div>
+  ) : (
+    <h3 onClick={() => setVideoCall(true)}>Start Call</h3>
   );
-}
+};
 
 export default App;
